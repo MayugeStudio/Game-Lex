8c706d8b8711e7073603039d9632f4881eda91c7
Mayuge <mayuge.studio.public@gmail.com>
2024-05-07 02:37:38 +0900

    Start developing !!

---
+++ src/text.c
@@ -0,0 +1,82 @@
+#include <assert.h>
+#include <string.h>
+#include <ctype.h>
+#include "text.h"
+
+
+GLX_text text_cstr_as_text(const char *cstr) {
+    return (GLX_text) {
+        .count = strlen(cstr),
+        .data = cstr,
+    };
+}
+
+GLX_text text_trim_left(GLX_text text) {
+    size_t i = 0;
+    while (i < text.count && isspace(*text.data)) {
+        i += 1;
+    }
+
+    return (GLX_text) {
+        .count =  text.count - i,
+        .data = text.data + i,
+    };
+}
+
+GLX_text text_trim_right(GLX_text text) {
+    size_t i = 0;
+    while (i < text.count && isspace(text.data[text.count - 1 - i])) {
+        i += 1;
+    }
+
+    return (GLX_text) {
+        .count = text.count - i,
+        .data = text.data,
+    };
+}
+
+GLX_text text_trim(GLX_text text) {
+    return text_trim_right(text_trim_left(text));
+}
+
+GLX_text text_chop_by_delim(GLX_text *text, char delim) {
+    size_t i = 0;
+    while (i < text->count && text->data[i] != delim) {
+        i += 1;
+    }
+
+    GLX_text result = {
+        .count = i,
+        .data = text->data,
+    };
+
+    if (i < text->count) {
+        // Skip delimiter
+        text->count -= i + 1;
+        text->data += i + 1;
+    } else {
+        // Doesn't skip delimiter
+        text->count -= i;
+        text->data += i;
+    }
+
+    return result;
+}
+
+int text_to_int(GLX_text text) {
+    int result = 0;
+
+    for (size_t i = 0; i < text.count && isdigit(*text.data); ++i) {
+        result = result * 10 + text.data[i] - '0';
+    }
+
+    return result;
+}
+
+int text_eq(GLX_text a, GLX_text b) {
+    if (a.count != b.count) {
+        return 0;
+    } else {
+        return memcmp(a.data, b.data, a.count) == 0;
+    }
+}
---
+++ src/text.h
@@ -0,0 +1,24 @@
+//
+// Created by eyebrow on 2024/05/06.
+//
+
+#ifndef TEXT_H
+#define TEXT_H
+
+#include <stddef.h>
+
+typedef struct
+{
+    size_t count;
+    const char *data;
+} GLX_text;
+
+GLX_text text_cstr_as_text(const char *cstr);
+GLX_text text_trim_left(GLX_text text);
+GLX_text text_trim_right(GLX_text text);
+GLX_text text_trim(GLX_text text);
+GLX_text text_chop_by_delim(GLX_text *text, char delim);
+int text_eq(GLX_text a, GLX_text b);
+int text_to_int(GLX_text text);
+
+#endif //TEXT_H
