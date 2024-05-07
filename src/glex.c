a17050a8e90e788e9516a7bc4dbeec51f65443ab
Mayuge <mayuge.studio.public@gmail.com>
2024-05-08 04:47:47 +0900

    Fix typo

--- src/glex.c
+++ src/glex.c
@@ -6,7 +6,7 @@ const char *err_as_cstr(Err err)
     switch (err) {
         case ERR_OK: return "ERR_OK";
         case ERR_STACK_OVERFLOW: return "ERR_STACK_OVERFLOW";
-        case ERR_STACK_UNDERFLOW: return "ERR_STACK_UNDER";
+        case ERR_STACK_UNDERFLOW: return "ERR_STACK_UNDERFLOW";
         case ERR_DIV_BY_ZERO: return "ERR_DIV_BY_ZERO";
         case ERR_ILLEGAL_INST: return "ERR_ILLEGAL_INST";
         case ERR_ILLEGAL_INST_ACCESS: return "ERR_ILLEGAL_INST_ACCESS";
